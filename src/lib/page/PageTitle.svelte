a1ae977b142b4ab7bd18bed05f24b9e6d57bbb11
Bohdan <bogdankf1@gmail.com>
2023-03-22 23:22:11 +0200

    fix: update PageTitle

--- src/lib/page/PageTitle.svelte
+++ src/lib/page/PageTitle.svelte
@@ -1,14 +1,28 @@
 <script lang="ts">
-	import { Link } from 'carbon-components-svelte';
-	import { Launch } from 'carbon-icons-svelte';
+	import { Button, Link } from 'carbon-components-svelte';
+	import { Launch, TrashCan, Upload } from 'carbon-icons-svelte';
 
 	export let title: string;
-	export let viewAllLink: string;
+	export let viewAllLink: string | undefined;
+	export let editLink: string | undefined;
+	export let deleteLink: string | undefined;
+	export let onSubmit: () => void;
 </script>
 
 <div class="w-full flex justify-between items-center mb-3">
 	<h2 class="text-xl">{title}</h2>
-	{#if viewAllLink}
-		<Link icon={Launch} href={viewAllLink}>View all</Link>
-	{/if}
+	<div class="flex items-center gap-3">
+		{#if onSubmit}
+			<Button icon={Upload} on:click={onSubmit}>Submit</Button>
+		{/if}
+		{#if editLink}
+			<Link icon={Launch} href={editLink}>Edit</Link>
+		{/if}
+		{#if deleteLink}
+			<Link icon={TrashCan} href={deleteLink}>Delete</Link>
+		{/if}
+		{#if viewAllLink}
+			<Link icon={Launch} href={viewAllLink}>View all</Link>
+		{/if}
+	</div>
 </div>
