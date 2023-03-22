c4717f4245830c0168c25b6ab1bd4e528b7359dc
Bohdan <bogdankf1@gmail.com>
2023-03-22 21:21:10 +0200

    feat: added FormWrapper component

---
+++ src/lib/forms/FormWrapper.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { Form } from 'carbon-components-svelte';
+
+	export let onSubmit: () => void;
+</script>
+
+<Form
+	on:submit={(e) => {
+		e.preventDefault();
+		onSubmit();
+	}}
+	class="flex items-center gap-3"
+>
+	<slot />
+</Form>
--- src/lib/index.js
+++ src/lib/index.js
@@ -2,3 +2,4 @@
 export { default as Button } from './buttons/Button.svelte';
 export { default as Table } from './tables/Table.svelte';
 export { default as Page } from './page/Page.svelte';
+export { default as FormWrapper } from './forms/FormWrapper.svelte';
