8c9ecd7a6a29dc0b38f749759fdd28d3f3b595e1
Bohdan <bogdankf1@gmail.com>
2023-03-22 22:53:00 +0200

    feat: added PageTitle component

--- src/lib/index.js
+++ src/lib/index.js
@@ -2,4 +2,5 @@
 export { default as Button } from './buttons/Button.svelte';
 export { default as Table } from './tables/Table.svelte';
 export { default as Page } from './page/Page.svelte';
+export { default as PageTitle } from './page/PageTitle.svelte';
 export { default as FormWrapper } from './forms/FormWrapper.svelte';
---
+++ src/lib/page/PageTitle.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { Link } from 'carbon-components-svelte';
+	import { Launch } from 'carbon-icons-svelte';
+
+	export let title: string;
+	export let viewAllLink: string;
+</script>
+
+<div class="w-full flex justify-between items-center mb-3">
+	<h2 class="text-xl">{title}</h2>
+	<Link icon={Launch} href={viewAllLink}>View all</Link>
+</div>
